de7b0d61a88f65b0e723a87a2a900f7fc45bcfe6
eonjugg <eonjugg@gmail.com>
2026-03-31 13:18:23 -0400

    Modify bootstrap script to ensure output doesn't contain characters besides `+*=[]()cehrx`

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,13 @@ python3 pyfuck.py pyfuck.py > bs_pyfuck.py
 
 # 2. Verify it works with hello world
 python3 bs_pyfuck.py examples/helloworld.py | python3 
+
+# 3. Verify bootstrap doesn't contain characters other than `+*=()[]cehrx`
+match=$(grep -o -b -m 1 -E '[^][+*=()cehrx]' bs_pyfuck.py)
+if [ -n "$match" ]; then
+    offset="${match%%:*}"
+    char="${match##*:}"
+    echo "Disallowed character '$char' found at byte $offset"
+else
+    echo "No other characters in output"
+fi
